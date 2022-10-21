4c6c2bfe1ee6103310cca138c522175bd2638f73
Dmitry <zeonlb426@gmail.com>
2022-10-22 02:11:53 +0300

    Add click to poster

--- src/Components/Poster.jsx
+++ src/Components/Poster.jsx
@@ -1,7 +1,18 @@
+import React, { useEffect, useState } from 'react';
+
 
 function Poster(props) {
 
-    const {loaded, movies, index} = props;
+    const {loaded, movies} = props;
+    const [index, setIndex] = useState(0);
+
+    useEffect(()=>{
+        setIndex(Math.floor(Math.random() * 19))
+    },[])
+
+    const changeHandel = (index) => {
+        setIndex(index);
+    }
 
     if ( !loaded ) {
         return <div className={'w-full h-[calc(100vh-200px)] bg-slate-300 flex items-center justify-center'}>Loading...</div>;
@@ -22,13 +33,13 @@ function Poster(props) {
                     </div>
                 </div>
                 <div className='absolute bottom-0 flex items-center'>
-                    {movies.map((movie, key) => (
-                        <a href='#'>
-                        <div className=''>
+                    {movies.map((movie, index) => (
+                        <button key={index} className="hover:scale-150 transition ease-in-out delay-350" onClick={() => changeHandel(index)}>
+                            <div className=''>
                                 <img className='' src={'https://image.tmdb.org/t/p/w220_and_h330_face'+ movie.poster_path} alt=''/>
                                 {/* <div className='Poster-average'>{movie.vote_average}</div> */}
                             </div>
-                        </a>
+                        </button>
                     ))}
                 </div>
             </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,17 +4,12 @@ import Poster from '../Components/Poster.jsx';
 
 export default function Home(props) {
   const {loaded, movies} = props;
-  const [index, setIndex] = useState(0);
-
-  useEffect(()=>{
-    setIndex(Math.floor(Math.random() * 19))
-  },[])
-
+  
   return (
     <>
       <Header />
       <div className='w-full'>
-        <Poster loaded={loaded} movies={movies} index={index}/>
+        <Poster loaded={loaded} movies={movies}/>
       </div>
     </>
   );
